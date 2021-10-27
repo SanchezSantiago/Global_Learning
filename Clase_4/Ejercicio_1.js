1d15cfee324bc9976838b34fbeda39dc145d35c6
Santiago Sanchez <santiagosancheznieva@gmail.com>
2021-10-27 09:38:05 -0300

    Se ha mejorado la legibidad del ejercicio_1.js de clase_4.

--- Clase_4/Ejercicio_1.js
+++ Clase_4/Ejercicio_1.js
@@ -1,15 +1,14 @@
 /*Santiago Sanchez Nieva - Ejercicios 1*/
+//Funcion
+const esPar = (num) => num % 2 == 0 ? console.log(`El numero ${num} es par`) : console.log(`El numero ${num} es impar`);
 
-function esPar(num){
-    if( num % 2 == 0){
-        console.log("El numero", num, "es par");
-    }
-    else{
-        console.log("El numero", num,"no es par");
-    }
-}
 
+//Pruebas
+esPar(8);
 esPar(5);
+esPar(105);
+esPar(10548);
+esPar(100000245);
 
 
 
